ca1221e3524c4b62b1944628a2a134a41aa8a340
Amin Khodabande <whfk233@gmail.com>
2022-12-04 15:12:28 +0330

    created API for getting questions

--- src/router/quiz/quiz.ts
+++ src/router/quiz/quiz.ts
@@ -26,4 +26,27 @@ router.post('/question', auth(1), async(req, res) => {
     }
 })
 
+router.get('/question', async(req, res) => {
+    try {
+        const skip = req.query.skip ? Number(req.query.skip) : 0
+        const limit = req.query.limit ? Number(req.query.limit) : 5
+
+        const questions = await Question.find({}).skip(skip).limit(limit)
+
+        res.send(questions)
+    } catch(e) {
+        if (e instanceof Error) res.status(400).send({ error: e.message })
+    }
+})
+
+router.get('/question/:id', async(req, res) => {
+    try {
+        const question = await Question.findById(req.params.id)
+        if (!question) throw new Error('Question not found')
+        res.send(question)
+    } catch (e) {
+        if (e instanceof Error) res.status(404).send({ error: e.message })
+    }
+})
+
 export default router
